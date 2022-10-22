43ab4289532b9d620dc19b559f8b0259cfd65e34
dticsistemas <dtic.sistemas.cns@gmail.com>
2022-10-21 21:17:51 -0400

    add husky

--- ControlDocumentoFactura.Aplicacion/Dtos/Clientes/ClienteDto.cs
+++ ControlDocumentoFactura.Aplicacion/Dtos/Clientes/ClienteDto.cs
@@ -1,20 +1,22 @@
-﻿ using ControlDocumentoFactura.Dominio.Models.Clientes.ValueObjects;
+﻿using ControlDocumentoFactura.Dominio.Models.Clientes.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ControlDocumentoFactura.Aplicacion.Dtos.Clientes {
-	public class ClienteDto {
-			public Guid Id { get; set; }
+namespace ControlDocumentoFactura.Aplicacion.Dtos.Clientes
+{
+	public class ClienteDto
+	{
+		public Guid Id { get; set; }
 		public String NombreCompleto { get; set; }
 		public String Name { get; set; }
-			public String LastName { get; set; }
+		public String LastName { get; set; }
 		public String Passport { get; set; }
-				public String NeedAssistance { get; set; }
+		public String NeedAssistance { get; set; }
 		public String Nit { get; set; }
 		public String Email { get; set; }
-			public String Phone { get; set; }
+		public String Phone { get; set; }
 	}
 }
--- ControlDocumentoFactura.Aplicacion/Dtos/Facturas/FacturaDto.cs
+++ ControlDocumentoFactura.Aplicacion/Dtos/Facturas/FacturaDto.cs
@@ -5,25 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ControlDocumentoFactura.Aplicacion.Dtos.Pagos {
-	public class FacturaDto {
+namespace ControlDocumentoFactura.Aplicacion.Dtos.Pagos
+{
+	public class FacturaDto
+	{
 		public Guid Id { get; set; }
 		public decimal Monto { get; set; }
-			public decimal Importe { get; set; }
+		public decimal Importe { get; set; }
 		public DateTime Fecha { get; set; }
 		public String NroFactura { get; set; }
 		public String Lugar { get; set; }
-			public String NitProveedor { get; set; }
+		public String NitProveedor { get; set; }
 		public String RazonSocialProveedor { get; set; }
 		public String NitBeneficiario { get; set; }
-			public String RazonSocialBeneficiario { get; set; }
+		public String RazonSocialBeneficiario { get; set; }
 		public String NroAutorizacion { get; set; }
 		public String Estado { get; set; }
 		public String TipoNit { get; set; }
-			public Guid ReservaId { get; set; }
+		public Guid ReservaId { get; set; }
 		public Guid ClienteId { get; set; }
 		public Guid VueloId { get; set; }
-		public Guid ConfiguracionFacturaId{ get; set; }
+		public Guid ConfiguracionFacturaId { get; set; }
 
 
 
