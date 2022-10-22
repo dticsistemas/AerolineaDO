f12c7e08ea883ff8db833b5f0de6cdcd5b5f8745
dticsistemas <dtic.sistemas.cns@gmail.com>
2022-10-21 21:07:16 -0400

    format husky

--- ControlDocumentoFactura.Aplicacion/Dtos/Clientes/ClienteDto.cs
+++ ControlDocumentoFactura.Aplicacion/Dtos/Clientes/ClienteDto.cs
@@ -1,4 +1,4 @@
-﻿using ControlDocumentoFactura.Dominio.Models.Clientes.ValueObjects;
+﻿ using ControlDocumentoFactura.Dominio.Models.Clientes.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace ControlDocumentoFactura.Aplicacion.Dtos.Clientes {
 	public class ClienteDto {
-		public Guid Id { get; set; }
+			public Guid Id { get; set; }
 		public String NombreCompleto { get; set; }
 		public String Name { get; set; }
-		public String LastName { get; set; }
+			public String LastName { get; set; }
 		public String Passport { get; set; }
-		public String NeedAssistance { get; set; }
+				public String NeedAssistance { get; set; }
 		public String Nit { get; set; }
 		public String Email { get; set; }
-		public String Phone { get; set; }
+			public String Phone { get; set; }
 	}
 }
